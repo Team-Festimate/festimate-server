20e81ec39952c8248c39dfeac28895d4a2722c7d
2hyunjinn <huenjin04@naver.com>
2025-02-09 18:29:08 +0900

    [init] #1 project setting

---
+++ src/main/java/com/festimate/festimate/FestimateApplication.java
@@ -0,0 +1,13 @@
+package com.festimate.festimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestimateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/festimate/festimate/FestimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festimate.festimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
