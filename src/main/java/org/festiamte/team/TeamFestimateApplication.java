75f70afb3f413094326bfd012ddca8ffab1fa66b
2hyunjinn <huenjin04@naver.com>
2025-02-26 18:30:37 +0900

    Initial commit

---
+++ src/main/java/org/festiamte/team/TeamFestimateApplication.java
@@ -0,0 +1,13 @@
+package org.festiamte.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFestimateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamFestimateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/festiamte/team/TeamFestimateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.festiamte.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFestimateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
