aeb1a750e37876a3db17b6508a2271d44dc9d8b1
2hyunjinn <huenjin04@naver.com>
2025-02-27 17:31:51 +0900

    [feat] #4 Matching Entity 설정

---
+++ src/main/java/org/festimate/team/matching/entity/MatchingStatus.java
@@ -0,0 +1,16 @@
+package org.festimate.team.matching.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum MatchingStatus {
+    COMPLETED("완료"),
+    PENDING("보류");
+
+    private final String description;
+
+    MatchingStatus(String description) {
+        this.description = description;
+    }
+
+}
