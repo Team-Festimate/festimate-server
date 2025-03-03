847ec3b0378e00e21733191973dbd4c3213bdd24
2hyunjinn <huenjin04@naver.com>
2025-03-03 20:34:58 +0900

    [feat] #14 AppearanceType 추가

---
+++ src/main/java/org/festimate/team/user/entity/AppearanceType.java
@@ -0,0 +1,16 @@
+package org.festimate.team.user.entity;
+
+public enum AppearanceType {
+    DOG("강아지상"),
+    CAT("고양이상"),
+    RABBIT("토끼상"),
+    FOX("여우상"),
+    BEAR("곰상"),
+    DINOSAUR("공룡상");
+
+    private final String description;
+
+    AppearanceType(String description) {
+        this.description = description;
+    }
+}
