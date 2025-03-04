c2c812d384e17aa016bdb4d644e99ef7adf7b40f
2hyunjinn <huenjin04@naver.com>
2025-03-05 01:40:26 +0900

    [feat] #18 jwt 를 통한 userId 조회 기능 구현

--- src/main/java/org/festimate/team/auth/jwt/JwtProvider.java
+++ src/main/java/org/festimate/team/auth/jwt/JwtProvider.java
@@ -1,33 +1,58 @@
 package org.festimate.team.auth.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtProvider {
     private final JwtProperties jwtProperties;
 
-    public String createAccessToken(String userId) {
+    private static final String USER_ID = "userId";
+
+    public String createAccessToken(Long userId) {
         return createToken(userId, jwtProperties.getAccessExpiration());
     }
 
-    public String createRefreshToken(String userId) {
+    public String createRefreshToken(Long userId) {
         return createToken(userId, jwtProperties.getRefreshExpiration());
     }
 
-    private String createToken(String userId, long expirationTime) {
+    private String createToken(Long userId, long expirationTime) {
+        SecretKey secretKey = getSecretKey();
+
         return Jwts.builder()
-                .setSubject(userId)
+                .setSubject(userId.toString())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+                .claim(USER_ID, userId)
+                .signWith(secretKey)
                 .compact();
+    }
+
+    public Long parseTokenAndGetUserId(String token) {
+        SecretKey secretKey = getSecretKey();
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.get(USER_ID, Long.class);
+    }
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(
+                jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8));
     }
 }
