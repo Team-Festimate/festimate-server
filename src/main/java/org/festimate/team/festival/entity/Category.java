0badca2c66dbf2e60a1fbcd8beaa4d516f82fdfc
2hyunjinn <huenjin04@naver.com>
2025-03-05 17:51:18 +0900

    [chore] #24 request category 자료형 변경

--- src/main/java/org/festimate/team/festival/entity/Category.java
+++ src/main/java/org/festimate/team/festival/entity/Category.java
@@ -4,4 +4,8 @@ public enum Category {
     LIFE,
     MUSIC,
     SCHOOL;
+
+    public static Category toCategory(String category) {
+        return Category.valueOf(category.toUpperCase());
+    }
 }
