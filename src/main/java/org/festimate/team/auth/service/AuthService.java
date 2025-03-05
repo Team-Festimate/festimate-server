c57f8e3ef0250f92c6956a1185dc000b68e45d9e
2hyunjinn <huenjin04@naver.com>
2025-03-05 16:37:22 +0900

    [chore] #22 회원가입, 로그인 겹치는 로직 통일

--- src/main/java/org/festimate/team/auth/service/AuthService.java
+++ src/main/java/org/festimate/team/auth/service/AuthService.java
@@ -3,8 +3,6 @@ package org.festimate.team.auth.service;
 import org.festimate.team.user.dto.TokenResponse;
 
 public interface AuthService {
-    TokenResponse login(Long userId);
-
-    TokenResponse signUp(Long userId);
+    TokenResponse generateTokens(Long userId);
 
 }
