629ab58d20e00cbb4f36125b8b76c8c2aecf5be6
2hyunjinn <huenjin04@naver.com>
2025-03-25 16:15:10 +0900

    [feat] #30 response body 에 카테고리 추가

--- src/main/java/org/festimate/team/user/dto/UserFestivalResponse.java
+++ src/main/java/org/festimate/team/user/dto/UserFestivalResponse.java
@@ -1,11 +1,13 @@
 package org.festimate.team.user.dto;
 
+import org.festimate.team.festival.entity.Category;
 import org.festimate.team.festival.entity.Festival;
 
 import java.time.format.DateTimeFormatter;
 
 public record UserFestivalResponse(
         long festivalId,
+        Category category,
         String title,
         String startDate,
         String endDate
@@ -15,6 +17,7 @@ public record UserFestivalResponse(
     public static UserFestivalResponse from(Festival festival) {
         return new UserFestivalResponse(
                 festival.getFestivalId(),
+                festival.getCategory(),
                 festival.getTitle(),
                 festival.getStartDate().format(FORMATTER),
                 festival.getEndDate().format(FORMATTER)
