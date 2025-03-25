4e9e7ce86468da78c06c8f425a73cf8c1a812398
2hyunjinn <huenjin04@naver.com>
2025-03-25 16:02:40 +0900

    [chore] #30 uri 수정

--- src/main/java/org/festimate/team/festival/controller/FestivalController.java
+++ src/main/java/org/festimate/team/festival/controller/FestivalController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/festival")
+@RequestMapping("/v1/festival")
 @RequiredArgsConstructor
 public class FestivalController {
 
