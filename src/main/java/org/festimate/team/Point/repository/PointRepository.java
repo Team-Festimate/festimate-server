731859c00facba7250a180cd4f3589db50836e53
2hyunjinn <huenjin04@naver.com>
2025-04-02 20:53:35 +0900

    [del] #52 불필요한 import 문 삭제

--- src/main/java/org/festimate/team/Point/repository/PointRepository.java
+++ src/main/java/org/festimate/team/Point/repository/PointRepository.java
@@ -1,7 +1,6 @@
 package org.festimate.team.Point.repository;
 
 import org.festimate.team.Point.entity.Point;
-import org.festimate.team.participant.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
