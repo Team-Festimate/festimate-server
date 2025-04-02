6b2fdcbd3eebb003ed33a20b9c47da01cfa986bb
2hyunjinn <huenjin04@naver.com>
2025-04-02 18:58:19 +0900

    [refactor] #49 Participant URI 수정

--- src/main/java/org/festimate/team/participant/controller/ParticipantController.java
+++ src/main/java/org/festimate/team/participant/controller/ParticipantController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/v1/participant")
+@RequestMapping("/v1/participants")
 @RequiredArgsConstructor
 public class ParticipantController {
 
