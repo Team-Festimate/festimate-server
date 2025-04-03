9ec09ed811a42f2c39e4863a6ffbe0ac7242f970
2hyunjinn <huenjin04@naver.com>
2025-04-03 23:51:39 +0900

    [feat] #54 포인트 차감 기능 구현

--- src/main/java/org/festimate/team/Point/service/impl/PointServiceImpl.java
+++ src/main/java/org/festimate/team/Point/service/impl/PointServiceImpl.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.festimate.team.Point.dto.PointHistory;
 import org.festimate.team.Point.dto.PointHistoryResponse;
+import org.festimate.team.Point.entity.Point;
+import org.festimate.team.Point.entity.TransactionType;
 import org.festimate.team.Point.repository.PointRepository;
 import org.festimate.team.Point.service.PointService;
+import org.festimate.team.common.response.ResponseError;
+import org.festimate.team.exception.FestimateException;
 import org.festimate.team.participant.entity.Participant;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -28,6 +33,23 @@ public class PointServiceImpl implements PointService {
     @Override
     public int getTotalPointByParticipant(Participant participant) {
         return pointRepository.getTotalPointByParticipant(participant.getParticipantId());
+    }
+
+    @Transactional
+    @Override
+    public void usePoint(Participant participant) {
+        int totalPoint = getTotalPointByParticipant(participant);
+        if (totalPoint < 1) {
+            throw new FestimateException(ResponseError.INSUFFICIENT_POINTS);
+        }
+
+        Point pointUsage = Point.builder()
+                .participant(participant)
+                .point(1)
+                .transactionType(TransactionType.DEBIT)
+                .build();
+
+        pointRepository.save(pointUsage);
     }
 
 }
