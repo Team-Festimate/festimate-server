573ab0235af82c84a3f4445a326e440f8c18cd56
2hyunjinn <huenjin04@naver.com>
2025-04-03 22:41:47 +0900

    [chore] #54 MatchingStatusResponse of 메소드 추가

--- src/main/java/org/festimate/team/matching/dto/MatchingStatusResponse.java
+++ src/main/java/org/festimate/team/matching/dto/MatchingStatusResponse.java
@@ -6,4 +6,7 @@ public record MatchingStatusResponse(
         MatchingStatus status,
         long matchingId
 ) {
+    public static MatchingStatusResponse of(MatchingStatus status, long matchingId) {
+        return new MatchingStatusResponse(status, matchingId);
+    }
 }
