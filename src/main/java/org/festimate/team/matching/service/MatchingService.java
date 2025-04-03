08c264254b173e6920b7c773db36fcd4da881e0d
2hyunjinn <huenjin04@naver.com>
2025-04-04 00:31:34 +0900

    [feat] #54 매칭 로직에서 중복 매칭 및 대기자 처리 기능 구현

--- src/main/java/org/festimate/team/matching/service/MatchingService.java
+++ src/main/java/org/festimate/team/matching/service/MatchingService.java
@@ -1,4 +1,15 @@
 package org.festimate.team.matching.service;
 
+import org.festimate.team.festival.entity.Festival;
+import org.festimate.team.matching.entity.Matching;
+import org.festimate.team.participant.entity.Participant;
+
+import java.util.Optional;
+
 public interface MatchingService {
+    Matching createMatching(Festival festival, Optional<Participant> targetParticipantOptional, Participant participant);
+
+    Optional<Participant> findMatchingCandidate(long festivalId, Participant participant);
+
+    void matchPendingParticipants(Participant newParticipant);
 }
--- src/main/java/org/festimate/team/matching/service/impl/MatchingServiceImpl.java
+++ src/main/java/org/festimate/team/matching/service/impl/MatchingServiceImpl.java
@@ -2,11 +2,56 @@ package org.festimate.team.matching.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.festimate.team.festival.entity.Festival;
+import org.festimate.team.matching.entity.Matching;
+import org.festimate.team.matching.entity.MatchingStatus;
+import org.festimate.team.matching.repository.MatchingRepository;
 import org.festimate.team.matching.service.MatchingService;
+import org.festimate.team.participant.entity.Participant;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class MatchingServiceImpl implements MatchingService {
+    private final MatchingRepository matchingRepository;
+
+    @Override
+    @Transactional
+    public Matching createMatching(Festival festival, Optional<Participant> targetParticipantOptional, Participant participant) {
+        Matching matching;
+
+        if (targetParticipantOptional.isEmpty()) {
+            matching = Matching.builder().festival(festival).applicantParticipant(participant).targetParticipant(null).status(MatchingStatus.PENDING).matchDate(LocalDateTime.now()).build();
+        } else {
+            matching = Matching.builder().festival(festival).applicantParticipant(participant).targetParticipant(targetParticipantOptional.get()).status(MatchingStatus.COMPLETED).matchDate(LocalDateTime.now()).build();
+        }
+
+        return matchingRepository.save(matching);
+    }
+
+    @Override
+    public Optional<Participant> findMatchingCandidate(long festivalId, Participant participant) {
+        return matchingRepository.findMatchingCandidate(participant.getParticipantId(), participant.getTypeResult(), participant.getUser().getGender(), festivalId);
+    }
+
+    @Transactional
+    @Override
+    public void matchPendingParticipants(Participant newParticipant) {
+        Optional<Matching> pendingMatchingOptional = matchingRepository.findFirstPendingMatchingByTypeAndOppositeGender(
+                newParticipant.getFestival().getFestivalId(),
+                newParticipant.getTypeResult(),
+                newParticipant.getUser().getGender()
+        );
+
+        pendingMatchingOptional.ifPresent(pendingMatching -> {
+            pendingMatching.completeMatching(newParticipant);
+        });
+    }
+
+
 }
