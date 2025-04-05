d19256ab81ef9973f460a8c3ef89247e09b9389e
2hyunjinn <huenjin04@naver.com>
2025-04-05 18:31:02 +0900

    [feat] #56 매칭 리스트 반환 로직 구현

--- src/main/java/org/festimate/team/matching/service/MatchingService.java
+++ src/main/java/org/festimate/team/matching/service/MatchingService.java
@@ -1,10 +1,12 @@
 package org.festimate.team.matching.service;
 
 import org.festimate.team.festival.entity.Festival;
+import org.festimate.team.matching.dto.MatchingInfo;
 import org.festimate.team.matching.entity.Matching;
 import org.festimate.team.participant.entity.Participant;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MatchingService {
@@ -14,4 +16,6 @@ public interface MatchingService {
     Optional<Participant> findBestCandidateByPriority(long festivalId, Participant participant);
 
     void matchPendingParticipants(Participant newParticipant);
+
+    List<MatchingInfo> getMatchingListByParticipant(Participant participant);
 }
