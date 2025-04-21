f4fb3ffe08e708b6a3783f5f6713ad412d43ce84
2hyunjinn <huenjin04@naver.com>
2025-04-21 17:24:48 +0900

    [feat] #91 Request Body 에 매칭 시작 일시 추가

--- src/main/java/org/festimate/team/festival/dto/FestivalRequest.java
+++ src/main/java/org/festimate/team/festival/dto/FestivalRequest.java
@@ -1,10 +1,12 @@
 package org.festimate.team.festival.dto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record FestivalRequest(
         String title,
         String category,
         LocalDate startDate,
-        LocalDate endDate
+        LocalDate endDate,
+        LocalDateTime matchingStartAt
 ){}
