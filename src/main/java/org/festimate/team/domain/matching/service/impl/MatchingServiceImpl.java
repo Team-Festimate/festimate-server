c00b2a036779b1f5cd333de0cf414ccc60a52faa
2hyunjinn <huenjin04@naver.com>
2025-05-06 01:48:00 +0900

    [fix] #110 없는 매칭일 경우 에러 발생

--- src/main/java/org/festimate/team/domain/matching/service/impl/MatchingServiceImpl.java
+++ src/main/java/org/festimate/team/domain/matching/service/impl/MatchingServiceImpl.java
@@ -74,7 +74,8 @@ public class MatchingServiceImpl implements MatchingService {
     public MatchingDetailInfo getMatchingDetail(Long userId, Long festivalId, Long matchingId) {
         Festival festival = festivalService.getFestivalByIdOrThrow(festivalId);
         participantService.getParticipantOrThrow(userService.getUserByIdOrThrow(userId), festival);
-        Matching matching = matchingRepository.findByMatchingId(matchingId);
+        Matching matching = Optional.ofNullable(matchingRepository.findByMatchingId(matchingId))
+                .orElseThrow(() -> new FestimateException(ResponseError.TARGET_NOT_FOUND));
         if (!matching.getFestival().getFestivalId().equals(festivalId)) {
             throw new FestimateException(ResponseError.FORBIDDEN_RESOURCE);
         }
