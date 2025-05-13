cc4d129c38b4b1ada8db9b42787ceb7ca0b15aa4
2hyunjinn <huenjin04@naver.com>
2025-05-13 19:48:25 +0900

    [feat] #119 다중 호스트용 엔티티 생성

---
+++ src/main/java/org/festimate/team/domain/festival/entity/FestivalHost.java
@@ -0,0 +1,55 @@
+package org.festimate.team.domain.festival.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.festimate.team.domain.user.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "festival_host")
+public class FestivalHost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long festivalHostId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "festival_id", nullable = false)
+    private Festival festival;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User host;
+
+    @Column(nullable = false)
+    private LocalDateTime addedAt;
+
+    @Builder
+    public FestivalHost(Festival festival, User host) {
+        this.festival = festival;
+        this.host = host;
+        this.addedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FestivalHost)) return false;
+        FestivalHost that = (FestivalHost) o;
+        return Objects.equals(getFestival(), that.getFestival()) &&
+                Objects.equals(getHost(), that.getHost());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFestival(), getHost());
+    }
+
+}
