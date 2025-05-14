8b95092cd20cf470d570b940f3df541f9f496993
2hyunjinn <huenjin04@naver.com>
2025-05-15 02:24:59 +0900

    [feat] #123 포인트 차감 시 잔여 포인트 보다 많이 차감할 경우 에러 발생

--- src/main/java/org/festimate/team/domain/point/service/impl/PointServiceImpl.java
+++ src/main/java/org/festimate/team/domain/point/service/impl/PointServiceImpl.java
@@ -4,13 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.festimate.team.api.point.dto.PointHistory;
 import org.festimate.team.api.point.dto.PointHistoryResponse;
+import org.festimate.team.domain.participant.entity.Participant;
 import org.festimate.team.domain.point.entity.Point;
 import org.festimate.team.domain.point.entity.TransactionType;
 import org.festimate.team.domain.point.repository.PointRepository;
 import org.festimate.team.domain.point.service.PointService;
-import org.festimate.team.global.response.ResponseError;
 import org.festimate.team.global.exception.FestimateException;
-import org.festimate.team.domain.participant.entity.Participant;
+import org.festimate.team.global.response.ResponseError;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -57,6 +57,10 @@ public class PointServiceImpl implements PointService {
     @Transactional
     @Override
     public void dischargePoint(Participant participant, int amount) {
+        int totalPoint = getTotalPointByParticipant(participant);
+        if (totalPoint < amount) {
+            throw new FestimateException(ResponseError.INSUFFICIENT_POINTS);
+        }
         Point pointUsage = createPointTransaction(participant, amount, TransactionType.DEBIT);
         pointRepository.save(pointUsage);
     }
