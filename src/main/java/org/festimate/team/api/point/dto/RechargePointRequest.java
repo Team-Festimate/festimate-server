3799d87f71412224f8570d30a20375abe5d33a27
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2025-05-15 02:37:13 +0900

    Revert "[feat] #123 TransactionType 추가"

--- src/main/java/org/festimate/team/api/point/dto/RechargePointRequest.java
+++ src/main/java/org/festimate/team/api/point/dto/RechargePointRequest.java
@@ -1,9 +1,6 @@
 package org.festimate.team.api.point.dto;
 
-import org.festimate.team.domain.point.entity.TransactionType;
-
 public record RechargePointRequest(
-        TransactionType type,
         long participantId,
         int point
 ) {
