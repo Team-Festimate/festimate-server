0e388c5cba528ac80e0cd16ea20552c45eafffb7
2hyunjinn <huenjin04@naver.com>
2025-06-12 21:49:12 +0900

    [refactor] #143 MatchingValidator에 매칭 관련 검증 로직 추가

--- src/main/java/org/festimate/team/domain/matching/validator/MatchingValidator.java
+++ src/main/java/org/festimate/team/domain/matching/validator/MatchingValidator.java
@@ -1,8 +1,11 @@
 package org.festimate.team.domain.matching.validator;
 
 import lombok.extern.slf4j.Slf4j;
-import org.festimate.team.global.response.ResponseError;
+import org.festimate.team.domain.festival.entity.Festival;
+import org.festimate.team.domain.matching.entity.Matching;
+import org.festimate.team.domain.participant.entity.Participant;
 import org.festimate.team.global.exception.FestimateException;
+import org.festimate.team.global.response.ResponseError;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -20,5 +23,17 @@ public class MatchingValidator {
         }
 
         log.info("matchingStartAt is valid.");
+    }
+
+    public static void isApplicantParticipantValid(final Matching matching, final Participant participant) {
+        if (matching.getApplicantParticipant() != participant) {
+            throw new FestimateException(ResponseError.FORBIDDEN_RESOURCE);
+        }
+    }
+
+    public static void isFestivalMatchValid(final Matching matching, final Festival festival) {
+        if (!matching.getFestival().getFestivalId().equals(festival.getFestivalId())) {
+            throw new FestimateException(ResponseError.FORBIDDEN_RESOURCE);
+        }
     }
 }
