aea748d92fe241568403a81c3bed4e42ee125872
2hyunjinn <huenjin04@naver.com>
2025-07-23 10:54:38 +0900

    [chore] #150 들여쓰기 수정

--- src/main/java/org/festimate/team/api/auth/dto/TokenResponse.java
+++ src/main/java/org/festimate/team/api/auth/dto/TokenResponse.java
@@ -1,8 +1,9 @@
 package org.festimate.team.api.auth.dto;
 
-public record TokenResponse(Long userId,
-                            String accessToken,
-                            String refreshToken
+public record TokenResponse(
+        Long userId,
+        String accessToken,
+        String refreshToken
 ) {
     public static TokenResponse of(Long userId, String accessToken, String refreshToken) {
         return new TokenResponse(userId, accessToken, refreshToken);
